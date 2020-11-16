0a188d11236d5dc222566b337be56f7834d13e5b
NielsJustesen <niels449@hotmail.com>
2020-11-16 14:12:41 +0100

    Massive flask/API issues

--- Bank/BankAPI.py
+++ Bank/BankAPI.py
@@ -1,22 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 import json
-from json import request
-from Functions import InterestRate, CreateLoan
+# from Functions import InterestRate, CreateLoan
 from Bank.BankDB import BankUser, Account, Deposit, Loan
 app = Flask(__name__)
 
 
-@app.route('/add-deposit', methods = ['POST'])
-def AddNewDeposit():
-    data = request.get_json()
-    amount = data["amount"]
-    bankUserId = data["BankUserId"]
-    if (amount <= 0 | amount == None):
-        return
-    else:
-        amountWithInterest = request.get_json(InterestRate.main(json.dumps({"amount":amount})))["amount"]
-        Account.UpdateAccount(bankUserId, amountWithInterest)
-        Deposit.AddDeposit(bankUserId, amount)
+# @app.route('/add-deposit', methods = ['POST'])
+# def AddNewDeposit():
+#     data = request.get_json()
+#     amount = data["amount"]
+#     bankUserId = data["BankUserId"]
+#     if (amount <= 0 | amount == None):
+#         return
+#     else:
+#         amountWithInterest = request.get_json(InterestRate.main(json.dumps({"amount":amount})))["amount"]
+#         Account.UpdateAccount(bankUserId, amountWithInterest)
+#         Deposit.AddDeposit(bankUserId, amount)
 
 @app.route('/list-deposits', methods = ['GET'])
 def ListDeposits():
@@ -27,18 +26,20 @@ def ListDeposits():
     if deposists:
         return { "deposits": deposists }, 200
 
-@app.route('/create-loan', methods = ['POST'])
-def CreateLoan():
-    loan = Loan()
-    data = request.get_json()
-    bankUserId = data["BankUserId"]
-    newAmount = data["NewAmount"]
-    currentAmount = data["CurrentAmount"]
-    validation = CreateLoan.main(json.dumps({"CurrentAmount": currentAmount, "NewAmount": newAmount}))
-    if (bankUserId == ""):
-        return 400
-    elif (validation["status code"] == 200):
-        loan.CreateLoan(bankUserId, newAmount)
-        return 201
-    else:
-        return { "Error Message": "insufficiant funds"}, 403
+# @app.route('/create-loan', methods = ['POST'])
+# def CreateLoan():
+#     loan = Loan()
+#     data = request.get_json()
+#     bankUserId = data["BankUserId"]
+#     newAmount = data["NewAmount"]
+#     currentAmount = data["CurrentAmount"]
+#     validation = CreateLoan.main(json.dumps({"CurrentAmount": currentAmount, "NewAmount": newAmount}))
+#     if (bankUserId == ""):
+#         return 400
+#     elif (validation["status code"] == 200):
+#         loan.CreateLoan(bankUserId, newAmount)
+#         return 201
+#     else:
+#         return { "Error Message": "insufficiant funds"}, 403
+
+app.run(port=4545)
--- Bank/BankDB.py
+++ Bank/BankDB.py
@@ -1,5 +1,8 @@
 import sqlite3
 from datetime import datetime, timedelta
+from flask import Flask, request, Response
+import json
+
 
 try:
     db = sqlite3.connect("Bank.db")
@@ -20,15 +23,17 @@ class BankUser():
 
     def GetBankUser(self, userId):
         try:
-            db.execute("SELECT * FROM BankUser WHERE Id = ?;", (str(userId)))
-            db.commit()
-            return db.cursor().fetchone()
+            cur = db.cursor()
+            cur.execute("SELECT * FROM BankUser WHERE Id = ?;", (str(userId)))
+            user = cur.fetchone()
+            return user
         except sqlite3.Error:
             print("--- FAILED TO SELECT BankUser ----")
 
 
-    def UpdateBankUser(self, userId, modifiedAt):
+    def UpdateBankUser(self, userId):
         try:
+            modifiedAt = datetime.now()
             db.execute("UPDATE BankUser SET ModifiedAt = ? WHERE Id = ?;", (str(modifiedAt), str(userId)))
             db.commit()
         except sqlite3.Error:
@@ -38,8 +43,10 @@ class BankUser():
         try:
             db.execute("DELETE FROM BankUser WHERE Id = ?;", (str(userId)))
             db.commit()
+            return True
         except:
             print("---- FAILED TO DELETE BankUser ---")
+            return False
 
 
 class Account():
@@ -64,9 +71,10 @@ class Account():
 
     def UpdateAccount(self, bankUserId, amount):
         try:
+            modifiedAt = datetime.now()
             oldAmount = self.GetAccount(bankUserId)[6]
             newAmount = amount + oldAmount
-            db.execute("UPDATE Account SET Amount = ? WHERE BankUserId = ?", (str(newAmount), str(bankUserId)))
+            db.execute("UPDATE Account SET Amount = ?, ModifiedAt = ? WHERE BankUserId = ?", (str(newAmount), str(modifiedAt), str(bankUserId)))
             db.commit()
         except sqlite3.Error:
             print("---- FAILED TO UPDATE ACCOUNT ----")
@@ -75,8 +83,10 @@ class Account():
         try:
             db.execute("DELETE FROM Account WHERE Id = ?;", (str(accountId)))
             db.commit()
+            return True
         except sqlite3.Error:
             print("---- FAILED TO DELETE ACCOUNT ----")
+            return False
 
 class Deposit():
 
@@ -96,8 +106,10 @@ class Deposit():
             cur.execute("SELECT amount FROM Deposit WHERE BankUserId = " + str(bankUserId))
             deposits = cur.fetchall()
             return deposits
-        except:
+        except sqlite3.Error as er:
             print("---- FAILED TO GET DEPOSITS ----")
+            print('SQLite error: %s' % (' '.join(er.args)))
+            print("Exception class is: ", er.__class__)
 
 class Loan():
 
@@ -113,7 +125,6 @@ class Loan():
             print("Exception class is: ", er.__class__)
                    
 
-
 bankUser = BankUser()
 account = Account()
 deposit = Deposit()
@@ -124,5 +135,73 @@ loan = Loan()
 # deposit.AddDeposit(1,1000)
 # userAccount = account.GetAccount(1)
 # print(account.GetAccount(1))
-# deposit.GetDeposits(1)
+deposits = deposit.GetDeposits(1)
+print(type(deposits[0]))
+for x in deposits:
+    print(x[0])
 # loan.CreateLoan(1, 200)
+# user = bankUser.GetBankUser(1)
+# bankUser.UpdateBankUser(1)
+# account.UpdateAccount(1, 1)
+# useraccunt = account.GetAccount(1)
+# useraccunt = account.DeleteAccount(1)
+# user = bankUser.DeleteBankUser(1)
+# print(user)
+# print(useraccunt)
+
+###############
+##### API #####
+###############
+
+# from Functions import InterestRate, CreateLoan
+
+
+app = Flask(__name__)
+
+
+# @app.route('/add-deposit', methods = ['POST'])
+# def AddNewDeposit():
+#     data = request.get_json()
+#     amount = data["amount"]
+#     bankUserId = data["BankUserId"]
+#     if (amount <= 0 | amount == None):
+#         return
+#     else:
+#         amountWithInterest = request.get_json(InterestRate.main(json.dumps({"amount":amount})))["amount"]
+#         Account.UpdateAccount(bankUserId, amountWithInterest)
+#         Deposit.AddDeposit(bankUserId, amount)
+
+@app.route('/list-deposits', methods = ['GET'])
+def ListDeposits():
+    # if 'BankUserId' in request.args:
+    #     bankUserId = request.args["BankUserId"]
+    bankUserId = 1
+    data = request.get_json()
+    # bankUserId = data["BankUserId"]
+    cur = db.cursor()
+    cur.execute("SELECT amount FROM Deposit WHERE BankUserId = " + str(bankUserId))
+    deposits = cur.fetchall()
+    returnObj = []
+    if deposits:
+        for x in deposits:
+            returnObj.append(deposits[0])
+    else:
+        return Response({"error": "WTF"}, 400)
+    if deposits:
+        return Response({ "deposits": json.dumps(returnObj) }, 200, mimetype='application/json')
+
+# @app.route('/create-loan', methods = ['POST'])
+# def CreateLoan():
+#     loan = Loan()
+#     data = request.get_json()
+#     bankUserId = data["BankUserId"]
+#     newAmount = data["NewAmount"]
+#     currentAmount = data["CurrentAmount"]
+#     validation = CreateLoan.main(json.dumps({"CurrentAmount": currentAmount, "NewAmount": newAmount}))
+#     if (bankUserId == ""):
+#         return 400
+#     elif (validation["status code"] == 200):
+#         loan.CreateLoan(bankUserId, newAmount)
+#         return 201
+#     else:
+#         return { "Error Message": "insufficiant funds"}, 403
