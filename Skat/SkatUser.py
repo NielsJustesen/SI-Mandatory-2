4eee9d17ceae9606b9b6cfe623c9c9e7ebdd19a3
ludvig <ludvigrandersen@gmail.com>
2020-11-21 10:07:04 +0100

    adds read SkatUser method

--- Skat/SkatUser.py
+++ Skat/SkatUser.py
@@ -22,8 +22,32 @@ def create():
     return {"status": f"SkatUser creation failed: {e}"}, 400
 
 @SkatUser.route('/skat-user', methods = ['GET'])
+# Find user with specified id
+# Returns SkatUser not found error if record does not exist
 def read():
-  pass
+  db = conn()
+
+  id = request.args.get('id')
+
+  db_cursor = db.cursor()
+  get_stmt = "SELECT * FROM SkatUser WHERE id=?"
+  
+  try:
+    db_cursor.execute(get_stmt, id)
+    SkatUser = db_cursor.fetchone()
+
+    if SkatUser is None:
+      return {"status": "SkatUser not found"}, 404
+    else:
+      user_json = {
+        "id": SkatUser[0],
+        "UserId": SkatUser[1],
+        "CreatdAt": SkatUser[2],
+        "IsActive": SkatUser[3]
+      }
+      return {"SkatUser": user_json}, 200
+  except Exception as e:
+    return {"status": f"failed getting SkatUser: {e}"}, 400
 
 @SkatUser.route('/skat-user', methods = ['PUT'])
 def update():
